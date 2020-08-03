21085e34e3c1229a9fc0820785c924d665e8df1b
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-08-03 17:53:14 -0600

    Almost

--- src/04_printing.py
+++ src/04_printing.py
@@ -3,7 +3,6 @@ Python provides a number of ways to perform printing. Research
 how to print using the printf operator, the `format` string 
 method, and by using f-strings.
 """
-
 x = 10
 y = 2.24552
 z = "I like turtles!"
@@ -12,6 +11,17 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+## 1.2 the 2 means how many deicmal point you want 
+# put the varible at the end in order how you want this to read.
+print('x is %d, y is %1.2f, z is "%s"' % (x, y, z))
+
 # Use the 'format' string method to print the same thing
+#helpful link https://www.youtube.com/watch?v=F3sxzkr715c
+print(f"x is {x}, y is {y}, z is '{z}'".format(x=x, y=round(y, 2),z=z))
+
 
 # Finally, print the same thing using an f-string
+#example -     print(f"Hello {name}")
+# how to i round? ANSWER = https://www.knowledgehut.com/blog/programming/python-rounding-numbers#:~:text=The%20function%20round()%20accepts,off%20a%20number%2C%20say%204.5.
+# round(number, number of digits)
+print(f"x is {x}, y is {round(y , 2)}, z is '{z}'")
--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,38 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+
+# append adds the object to the end of the list! 
+# WOKRING
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+
+#The EXTEND method adds the contents of one list to the end of another list.
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+#DEL() method deletes an element from the list at the index we pass in.
+del x[4]
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+#This method inserts an element at a specified position.
+
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+# This method calculates the total length of a list that we pass in as an argument.
+print(len(x))
 
+
+"working on later" 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
